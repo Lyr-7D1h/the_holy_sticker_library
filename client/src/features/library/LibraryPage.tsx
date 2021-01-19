6ef2dcaa8877c4acbcd66ca762afb65dea78af43
Lyr-7D1h <lyr-7d1h@pm.me>
2021-01-19 22:15:32 +0100

    Showing stickers in home

--- client/src/features/library/LibraryPage.tsx
+++ client/src/features/library/LibraryPage.tsx
@@ -5,9 +5,25 @@ import { useRootSelector } from "../../store"
 import Page from "../shared/Page"
 import { getStickers } from "./librarySlice"
 import { Sticker } from "@shared/types/library"
+import { fade, Grid, makeStyles } from "@material-ui/core"
+
+const useStyles = makeStyles((theme) => ({
+  gridItem: {
+    zIndex: 500,
+    // TODO: fix hover
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.8),
+    },
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+}))
 
 const LibraryPage: FC = () => {
   const [selected, setSelected] = useState("All")
+  const classes = useStyles()
   const dispatch = useDispatch()
   const drawerItems = [
     { title: "All", icon: <LibraryBooks /> },
@@ -23,7 +39,23 @@ const LibraryPage: FC = () => {
 
   return (
     <Page drawer={{ items: drawerItems, selected, onChange: setSelected }}>
-      Library
+      <Grid container spacing={1}>
+        {stickers.map((sticker) => (
+          <Grid
+            className={classes.gridItem}
+            key={sticker.hash}
+            item
+            sm={1}
+            xs={12}
+            spacing={3}
+          >
+            <img
+              className={classes.image}
+              src={`/resources/stickers/${sticker.hash}.webp`}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </Page>
   )
 }
--- shared/types/library.ts
+++ shared/types/library.ts
@@ -1,3 +1,4 @@
 export interface Sticker {
-  id: string
+  id: number
+  hash: string
 }
